6a399ace1f1560345bb47902631e4ceacf2abd54
sayf-ismail <63899527+sayf-ismail@users.noreply.github.com>
2024-01-16 12:51:55 +0800

    add interactive animation

--- app/treemap/page.tsx
+++ app/treemap/page.tsx
@@ -26,47 +26,142 @@ export default function TreeMapPage() {
       dimensions.height,
       dimensions.width,
     ]);
-    root = treeLayout(root);
 
-    console.log(root.descendants());
-    console.log(root.links());
+    // animation
+    var duration = 750;
+    var i = 0;
+    function update(source: any) {
+      var treeData = treeLayout(root);
 
-    const linkGenerator = linkHorizontal()
-      .x((node: any) => node.y)
-      .y((node: any) => node.x);
+      // nodes
+      var nodes = treeData.descendants();
+      nodes.forEach(function (d: any) {
+        d.y = d.depth * 180;
+      });
+      var node = svg.selectAll("g.node").data(nodes, function (d: any) {
+        return d.id || (d.id = ++i);
+      });
 
-    // nodes
-    svg
-      .selectAll(".node")
-      .data(root.descendants())
-      .join("circle")
-      .attr("class", "node")
-      .attr("r", 6)
-      .attr("fill", "black")
-      .attr("cx", (node: any) => node.y)
-      .attr("cy", (node: any) => node.x);
+      var nodeEnter = node
+        .enter()
+        .append("g")
+        .attr("class", "node")
+        .attr("transform", function (d: any) {
+          return "translate(" + source.y + "," + source.x + ")";
+        })
+        .on("click", click);
 
-    // links
-    svg
-      .selectAll(".link")
-      .data(root.links())
-      .join("path")
-      .attr("class", "link")
-      .attr("fill", "none")
-      .attr("stroke", "black")
-      .attr("d", linkGenerator as any);
+      nodeEnter
+        .append("circle")
+        .attr("class", "node")
+        .attr("r", 0)
+        .style("fill", function (d: any) {
+          return d._children ? "red" : "#fff";
+        });
 
-    // labels
-    svg
-      .selectAll(".label")
-      .data(root.descendants())
-      .join("text")
-      .attr("class", "label")
-      .attr("text-anchor", "middle")
-      .attr("font-size", 24)
-      .attr("x", (node: any) => node.y)
-      .attr("y", (node: any) => node.x - 20)
-      .text((node: any) => node.data.name);
+      nodeEnter
+        .append("text")
+        .attr("x", function (d: any) {
+          return d.children || d._children ? -13 : 13;
+        })
+        .attr("dy", ".35em")
+        .attr("text-anchor", function (d: any) {
+          return d.children || d._children ? "end" : "start";
+        })
+        .text(function (d: any) {
+          return d.data.name;
+        });
+
+      var nodeUpdate = nodeEnter.merge(node as any);
+
+      nodeUpdate
+        .transition()
+        .duration(duration)
+        .attr("transform", function (d: any) {
+          return "translate(" + d.y + "," + d.x + ")";
+        });
+
+      nodeUpdate
+        .select("circle.node")
+        .attr("r", 10)
+        .style("fill", function (d: any) {
+          return d._children ? "red" : "black";
+        })
+        .attr("cursor", "pointer");
+
+      var nodeExit = node
+        .exit()
+        .transition()
+        .duration(duration)
+        .attr("transform", function (d: any) {
+          return "translate(" + source.y + "," + source.x + ")";
+        })
+        .remove();
+
+      nodeExit.select("circle").attr("r", 0);
+      nodeExit.select("text").style("fill-opacity", 0);
+
+      // links
+      function diagonal(s: any, d: any) {
+        return `M ${s.y} ${s.x}
+                  C ${(s.y + d.y) / 2} ${s.x},
+                    ${(s.y + d.y) / 2} ${d.x},
+                    ${d.y} ${d.x}`;
+      }
+
+      var links = treeData.descendants().slice(1);
+      var link = svg.selectAll("path.link").data(links, function (d: any) {
+        return d.id;
+      });
+
+      var linkEnter = link
+        .enter()
+        .insert("path", "g")
+        .attr("class", "link")
+        .attr("d", function (d: any) {
+          var o = { x: source.x, y: source.y };
+          return diagonal(o, o);
+        });
+
+      var linkUpdate = linkEnter.merge(link as any);
+      linkUpdate
+        .transition()
+        .attr("fill", "none")
+        .attr("stroke", "black")
+        .duration(duration)
+        .attr("d", function (d: any) {
+          return diagonal(d, d.parent);
+        });
+
+      var linkExit = link
+        .exit()
+        .transition()
+        .duration(duration)
+        .attr("d", function (d: any) {
+          var o = { x: source.x, y: source.y };
+          return diagonal(o, o);
+        })
+        .remove();
+
+      // for transitions, need to store old positions
+
+      nodes.forEach(function (d: any) {
+        d.oldX = d.x;
+        d.oldY = d.y;
+      });
+
+      function click(event: any, d: any) {
+        if (d.children) {
+          d._children = d.children;
+          d.children = null;
+        } else {
+          d.children = d._children;
+          d._children = null;
+        }
+        update(d);
+      }
+    }
+    update(root);
   }, [transformedData, dimensions]);
 
   return (
